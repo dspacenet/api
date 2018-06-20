90ee43b1f523c37f17760922304d9369572df812
KinIcy NET <nkinicy@gmail.com>
2018-06-20 19:34:45 +0200

    now using sculp-parser to static check programs

--- sccpClient.js
+++ sccpClient.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
+const { SculpParser, Expressions } = require('sculp-parser');
 
 // Create HTTP Client to consume the SCCP API
 const sccpClient = axios.create({ baseURL: `http://localhost:${process.env.SCCP_PORT || 8082}/` });
 
 /**
  * Alters [program] to run inside the given [path].
- * @param {String} program - A SCULP/SCCP program to be runned.
- * @param {String} path - A string contining the path to the space where the
- * program will be runned.
+ * @param {String} program - A SCULP/SCCP program to be executed.
+ * @param {String} path - A string containing the path to the space where the
+ * program will be executed.
  */
 function spaceWrap(program, path) {
   // If path is empty, no alteration is done.
@@ -19,13 +20,19 @@ function spaceWrap(program, path) {
 
 /**
  * Run the given [program] owned by [user] in the given [path].
- * @param {String} program - program to be runned.
- * @param {String} path - path to the space where the program will be runned.
+ * @param {String} program - program to be executed.
+ * @param {String} path - path to the space where the program will be executed.
  * @param {String} user - owner of the program.
  */
 async function runSCCP(program, path, user, timeu = 1) {
+  // Parse the program to check syntax
+  const parser = new SculpParser(program);
+  // Add user to vote calls
+  parser.applyTo(Expressions.Procedure, (procedure) => { if (procedure.name === 'vote') procedure.pushParam(user); });
+  // rebuild program with update source code
+  const parsedProgram = parser.result.toString();
   // Path the program to post it's source code to the top of the given path
-  const patchedProgram = `${program} || enter @ "top" do post("${encodeURI(program)}")`;
+  const patchedProgram = `${parsedProgram} || enter @ "top" do post("${encodeURI(parsedProgram)}")`;
   // Call the SCCP API with the given program, path and user.
   const { data } = await sccpClient.post('runsccp', { config: spaceWrap(patchedProgram, path), user, timeu });
   // If result is 'error', throw error messages in result.errors
@@ -53,9 +60,9 @@ async function getSpace(path, filter = true) {
     error.status = 400;
     throw error;
   }
-  // If not filter, send messages without fiiltering.
+  // If not filter, send messages without filtering.
   if (!filter) return data.result;
-  // Filter system and private messages before sendind it
+  // Filter system and private messages before sending it
   return data.result.filter(post => post.class !== 'system' && post.usr_msg !== 'private');
 }
 
